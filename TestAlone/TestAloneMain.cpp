1239decea3846d571bf6c211f78081a6ee2c8bd3
prgrmz07 <prgrmz07@163.com>
2023-08-03 16:47:24 +0800

    chatgpt原草稿/独立测试主程序，源码文本写在代码中

--- TestAlone/TestAloneMain.cpp
+++ TestAlone/TestAloneMain.cpp
@@ -20,41 +20,22 @@ using namespace clang;
 using namespace clang;
 
 
-
-int main(int Argc, const char **Argv  ) {
-
-  // 获取编译配置
-  clang::tooling::CompilationDatabase &Compilations =  * new clang::tooling::FixedCompilationDatabase(Twine("."), std::vector<std::string>());
-
-  // 创建 CompilerInstance 对象
-  clang::CompilerInstance CI;
-  // 创建源管理器
-  CI.createDiagnostics();
-  CI.createFileManager();
-  CI.createSourceManager(CI.getFileManager());
-/* 确保在创建编译器实例（`clang::CompilerInstance`）后，立即创建源管理器（`clang::SourceManager`）并将其设置到编译器实例中
-否则 ，运行时 断言失败 报错 ：   CompilerInstance.h:423: clang::SourceManager& clang::CompilerInstance::getSourceManager() const: Assertion `SourceMgr && "Compiler instance has no source manager!"' failed.
- */
-
-  // 设置语言选项
-  CI.getLangOpts().CPlusPlus = true;
-
-  SourceManager& SM=CI.getSourceManager();
-
-
+int main() {
+  // 假设您已经有一个源码字符串，名为Code
   std::string Code = "int x = 5;";
 
-  clang::FileID MainFileID = CI.getSourceManager().createFileID(
+  // 创建一个SourceManager对象
+  SourceManager SM;
+
+  // 将源码字符串添加到SourceManager中
+  FileID File = SM.createFileIDForMemBuffer(
           llvm::MemoryBuffer::getMemBuffer(Code), clang::SrcMgr::C_User);
 
-
-
-
   // 获取源码字符串的起始位置
-  SourceLocation StartLoc = SM.getLocForStartOfFile(MainFileID);
+  SourceLocation StartLoc = SM.getLocForStartOfFile(File);
 
   // 获取源码字符串的结束位置
-  SourceLocation EndLoc = SM.getLocForEndOfFile(MainFileID);
+  SourceLocation EndLoc = SM.getLocForEndOfFile(File);
 
   // 获取源码字符串的token的结束位置
   SourceLocation TokenEndLoc = Lexer::getLocForEndOfToken(EndLoc, 0, SM, LangOptions());
@@ -62,7 +43,7 @@ int main(int Argc, const char **Argv  ) {
   // 输出结果
   std::cout << "Code: " << Code << std::endl;
   std::cout << "Token end location: ";
-//  TokenEndLoc.print(std::cout, SM);
+  TokenEndLoc.print(std::cout, SM);
   std::cout << std::endl;
 
   return 0;
