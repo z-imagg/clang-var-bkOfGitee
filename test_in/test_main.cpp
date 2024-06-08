ec2d064baf0835538ee18602d86a17141ecfcbd6
prgrmz07 <prgrmz07@163.com>
2024-06-08 20:09:11 +0800

    为了更简单 而 对待Lambda的原则: Lambda函数中只用自己内的_vdLs不用任何外部的_vdLs
    Lambda函数不能使用使用定义他的函数中的_vdLs、尽量避免使用执行他的函数中的_vdLs以保持简单 即Lambda函数中只用自己内的_vdLs不用任何外部的_vdLs

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -2,6 +2,11 @@
 #include <algorithm>
 #include <list>
 
+void funcEatLambdaFunc01(my_nsp::MyClass *(*func01)(my_nsp::UserEntity *)){
+  my_nsp::ScoreRelation sR1;
+  func01(nullptr);
+  return;
+}
 
 int main(int argc, char** argv){
   my_nsp::MyClass varMyClass;
@@ -17,6 +22,10 @@ int main(int argc, char** argv){
   std::list<my_nsp::MyClass*> ls;
 
   std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
+
+  //lambda函数lambdaFunc01 是在当前函数main中定义的 但在另一个函数funcEatLambdaFunc01中执行 因此Lambda函数不能使用使用定义他的函数中的_vdLs、尽量避免使用执行他的函数中的_vdLs以保持简单 即Lambda函数中只用自己内的_vdLs不用任何外部的_vdLs
+  my_nsp::MyClass *(*lambdaFunc01)(my_nsp::UserEntity *) = [](my_nsp::UserEntity *uEk) {  my_nsp::UserEntity *uEi = uEk;   return (my_nsp::MyClass *)uEk; };
+  funcEatLambdaFunc01(lambdaFunc01);
 
   return 0;
 }
