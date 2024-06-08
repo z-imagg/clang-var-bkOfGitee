7aaf257465d85c326b228b23b812c95af3dac6bd
prgrmz07 <prgrmz07@163.com>
2024-06-08 21:39:08 +0800

    Revert "开发日志 以 排查TraverseLambdaExpr跳过原因"
    This reverts commit f42b058c2a2c31e9244b393a65d10cad142e7e0f.

--- lib/Var/FnVst.cpp
+++ lib/Var/FnVst.cpp
@@ -231,15 +231,13 @@ bool FnVst::I__TraverseCXXMethodDecl(CXXMethodDecl* cxxMethDecl,const char* who)
       );
 }
 bool FnVst::TraverseLambdaExpr(LambdaExpr *lambdaExpr) {
-  if(sizeof(lambdaExpr)<0){
-    std::cout<<fmt::format("TraverseLambdaExpr跳过代号1");
+  if(sizeof(lambdaExpr)<0){//以这样一句话暂时跳过lambda
     return false;
   }
 
   //跳过非MainFile
   bool _LocFileIDEqMainFileID=UtilMainFile::LocFileIDEqMainFileID(SM,lambdaExpr->getBeginLoc());
   if(!_LocFileIDEqMainFileID){
-    std::cout<<fmt::format("TraverseLambdaExpr跳过代号2");
     return false;
   }
   //跳过 default
@@ -258,7 +256,6 @@ TraverseLambdaExpr跳过代号3
         capture.getCaptureKind() == clang::LCK_ByCopy ||
         capture.getCaptureKind() == clang::LCK_ByRef
     ) {
-      std::cout<<fmt::format("TraverseLambdaExpr跳过代号3");
       return false;
     }
   }
@@ -273,7 +270,6 @@ TraverseLambdaExpr跳过代号3
   //跳过 函数体内无语句
   int stmtCntInFuncBody= UtilCompoundStmt::childrenCntOfCompoundStmt(compoundStmt);
   if(stmtCntInFuncBody<=0){
-    std::cout<<fmt::format("TraverseLambdaExpr跳过代号4");
     return false;
   }
 
@@ -281,7 +277,6 @@ TraverseLambdaExpr跳过代号3
   //  lambda表达式写在一行是很常见的, 因此要求该lambda表达式体内无语句时才跳过
   bool funcBodyLRBraceInSameLine=UtilLineNum::isEqSrcLocLineNum(SM,funcBodyLBraceLoc,funcBodyRBraceLoc);
   if(funcBodyLRBraceInSameLine && stmtCntInFuncBody == 0){
-    std::cout<<fmt::format("TraverseLambdaExpr跳过代号5");
     return false;
   }
 
