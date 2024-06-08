5dbabaa425cc17d1a0ca40cfee658a35331e3c70
prgrmz07 <prgrmz07@163.com>
2024-06-08 19:50:52 +0800

    lambda测试材料

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,17 +1,23 @@
 #include "MyClz.h"
-
+#include <algorithm>
+#include <list>
 
 
 int main(int argc, char** argv){
-    my_nsp::MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
-    {
-        struct Point * ptr1=&pnt1;
-        struct Point * ptr2=&pnt2;
-        struct Point pnt3;
-    }
+  my_nsp::MyClass varMyClass;
+  static Point pnt1;
+  struct Point pnt2;
+  {
+    struct Point * ptr1=&pnt1;
+    struct Point * ptr2=&pnt2;
+    struct Point pnt3;
+  }
 
-    return 0;
+
+  std::list<my_nsp::MyClass*> ls;
+
+  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
+
+  return 0;
 }
 
