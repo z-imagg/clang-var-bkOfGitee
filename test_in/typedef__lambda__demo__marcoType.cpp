2bd19f6b2e50aab204f7d7b78416f61988401ea0
prgrmz07 <prgrmz07@163.com>
2024-06-09 17:29:41 +0800

    2 测试材料 变态点

--- test_in/typedef__lambda__demo__marcoType.cpp
+++ test_in/typedef__lambda__demo__marcoType.cpp
@@ -36,12 +36,15 @@ void func_demo_typedef(){
   TypeAlias_deepth2_Macro2_T_Clz_User userVar_class;
   TypeAlias_deepth5_Macro_T_Union_Score scoreVar_union;
   short short_var;
-  TypeAlias_deepth1_User  zhangsan;
+  TypeAlias_deepth1_User  zhangsan_user,LISI_user,wangwu_user;
   TypeAlias_deepth3_int age;
+  TypeAlias_deepth5_Macro_T_Union_Score scoreVar2_union,*ptr_scoreVar3_union;
+  TypeAlias_deepth2_Macro2_T_Clz_User userVar2_class,userVar3_class;
   return;
 }
 
 void func_demo_lambda(){
+  TypeAlias_deepth2_Macro2_T_Clz_User  userVarArray_class[30],userVar7_class;
   TypeAlias_deepth1_Macro_T_char ch_var;
   float k;
   auto lambda_func_var_1=[&k]( ){ return 0.0;};
