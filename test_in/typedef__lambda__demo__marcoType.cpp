612f4fb74e06873b13196d141d78231143146c3c
prgrmz07 <prgrmz07@163.com>
2024-06-09 17:26:38 +0800

    测试材料 变态点

--- test_in/typedef__lambda__demo__marcoType.cpp
+++ test_in/typedef__lambda__demo__marcoType.cpp
@@ -1,5 +1,26 @@
 #define Macro_T_Char char
 
+class Clz_User{
+
+};
+
+union _Union_Score {
+int scoreInt;
+float scoreFloat;
+};
+
+#define Macro_T_Clz_User Clz_User
+#define Macro2_T_Clz_User Macro_T_Clz_User
+typedef Macro2_T_Clz_User TypeAlias_deepth1_Macro2_T_Clz_User;
+typedef TypeAlias_deepth1_Macro2_T_Clz_User TypeAlias_deepth2_Macro2_T_Clz_User;
+
+#define Macro_T_Union_Score _Union_Score
+typedef Macro_T_Union_Score TypeAlias_deepth1_Macro_T_Union_Score;
+typedef TypeAlias_deepth1_Macro_T_Union_Score TypeAlias_deepth2_Macro_T_Union_Score;
+typedef TypeAlias_deepth2_Macro_T_Union_Score TypeAlias_deepth3_Macro_T_Union_Score;
+typedef TypeAlias_deepth3_Macro_T_Union_Score TypeAlias_deepth4_Macro_T_Union_Score;
+typedef TypeAlias_deepth4_Macro_T_Union_Score TypeAlias_deepth5_Macro_T_Union_Score;
+
 typedef struct _RealType_User_{
   int id;
   char* name;
@@ -12,6 +33,8 @@ typedef TypeAlias_deepth1_int TypeAlias_deepth2_int;
 typedef TypeAlias_deepth2_int TypeAlias_deepth3_int;
 
 void func_demo_typedef(){
+  TypeAlias_deepth2_Macro2_T_Clz_User userVar_class;
+  TypeAlias_deepth5_Macro_T_Union_Score scoreVar_union;
   short short_var;
   TypeAlias_deepth1_User  zhangsan;
   TypeAlias_deepth3_int age;
