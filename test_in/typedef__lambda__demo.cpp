636814427b7b4cc3a4d70b008f53aac17cf76cc5
prgrmz07 <prgrmz07@163.com>
2024-06-09 15:06:07 +0800

    测试材料 lamba typedef整合到一起

--- test_in/typedef__lambda__demo.cpp
+++ test_in/typedef__lambda__demo.cpp
@@ -8,8 +8,16 @@ typedef int TypeAlias_deepth1_int;
 typedef TypeAlias_deepth1_int TypeAlias_deepth2_int;
 typedef TypeAlias_deepth2_int TypeAlias_deepth3_int;
 
-void func01(){
+void func_demo_typedef(){
   TypeAlias_deepth1_User  zhangsan;
   TypeAlias_deepth3_int age;
+  return;
+}
+
+void func_demo_lambda(){
+  float k;
+  auto lambda_func_var_1=[&k]( ){ return 0.0;};
+  double (*lambda_func_var_2)() = []() { return 0.0; };
+  int float_auto_var2=9.0;
   return;
 }
