c9a0006074127c7c9ac4551d676a5cf94ad59e9a
prgrmz07 <prgrmz07@163.com>
2024-06-09 14:02:44 +0800

    测试材料

--- test_in/lambda__demo.cpp
+++ test_in/lambda__demo.cpp
@@ -3,6 +3,6 @@ void func_demo(){
   float k;
   auto lambda_func_var_1=[&k]( ){ return 0.0;};
   double (*lambda_func_var_2)() = []() { return 0.0; };
-  auto float_auto_var2=9.0;
+  int float_auto_var2=9.0;
   return;
 }
