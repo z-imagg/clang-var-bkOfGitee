a52154046b4bdd86bf7486d2a462cfa0e70fc564
prgrmz07 <prgrmz07@163.com>
2024-06-09 15:45:01 +0800

    测试材料 typedef指向类型的宏别名

--- test_in/typedef__lambda__demo__marcoType.cpp
+++ test_in/typedef__lambda__demo__marcoType.cpp
@@ -1,3 +1,5 @@
+#define Macro_T_Char char
+
 typedef struct _RealType_User_{
   int id;
   char* name;
@@ -5,6 +7,7 @@ typedef struct _RealType_User_{
 } TypeAlias_deepth1_User;
 
 typedef int TypeAlias_deepth1_int;
+typedef Macro_T_Char TypeAlias_deepth1_Macro_T_char;
 typedef TypeAlias_deepth1_int TypeAlias_deepth2_int;
 typedef TypeAlias_deepth2_int TypeAlias_deepth3_int;
 
@@ -15,6 +18,7 @@ void func_demo_typedef(){
 }
 
 void func_demo_lambda(){
+  TypeAlias_deepth1_Macro_T_char ch_var;
   float k;
   auto lambda_func_var_1=[&k]( ){ return 0.0;};
   double (*lambda_func_var_2)() = []() { return 0.0; };
