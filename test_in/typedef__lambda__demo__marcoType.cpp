ae269fe1b637da892ac199887712bf020457497f
prgrmz07 <prgrmz07@163.com>
2024-06-09 20:12:47 +0800

    记录问题 ， 测试材料

--- test_in/typedef__lambda__demo__marcoType.cpp
+++ test_in/typedef__lambda__demo__marcoType.cpp
@@ -33,22 +33,10 @@ typedef TypeAlias_deepth1_int TypeAlias_deepth2_int;
 typedef TypeAlias_deepth2_int TypeAlias_deepth3_int;
 
 void func_demo_typedef(){
-  TypeAlias_deepth2_Macro2_T_Clz_User userVar_class;
-  TypeAlias_deepth5_Macro_T_Union_Score scoreVar_union;
-  short short_var;
   TypeAlias_deepth1_User  zhangsan_user,LISI_user,wangwu_user;
-  TypeAlias_deepth3_int age;
-  TypeAlias_deepth5_Macro_T_Union_Score scoreVar2_union,*ptr_scoreVar3_union;
-  TypeAlias_deepth2_Macro2_T_Clz_User userVar2_class,userVar3_class;
   return;
 }
 
 void func_demo_lambda(){
-  TypeAlias_deepth2_Macro2_T_Clz_User  userVarArray_class[30],userVar7_class;
-  TypeAlias_deepth1_Macro_T_char ch_var;
-  float k;
-  auto lambda_func_var_1=[&k]( ){ return 0.0;};
-  double (*lambda_func_var_2)() = []() { return 0.0; };
-  int float_auto_var2=9.0;
   return;
 }
