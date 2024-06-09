0b03147ac52477ddad3ab7b2b00ea0ba6d468649
prgrmz07 <prgrmz07@163.com>
2024-06-09 14:04:22 +0800

    测试材料中各个VarDecl大致样子

--- test_in/lambda__demo.cpp
+++ test_in/lambda__demo.cpp
@@ -6,3 +6,13 @@ void func_demo(){
   int float_auto_var2=9.0;
   return;
 }
+/**
+
+/fridaAnlzAp/clang-var/cmake-build-debug/bin/VarAlone /fridaAnlzAp/clang-var/test_in/lambda__demo.cpp
+输出如下
+ 
+typeName='float',typeClass=11,typeClassName=Builtin,isBuiltinType=true
+typeName='class (lambda at /fridaAnlzAp/clang-var/test_in/lambda__demo.cpp:4:26)',typeClass=14,typeClassName=Auto,isBuiltinType=false
+typeName='double (*)(void)',typeClass=37,typeClassName=Pointer,isBuiltinType=false
+typeName='int',typeClass=11,typeClassName=Builtin,isBuiltinType=true
+*/
