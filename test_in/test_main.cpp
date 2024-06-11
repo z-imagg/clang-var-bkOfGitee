468d63161a31ccda8bb1933d1934f23f497f978f
prgrmz07 <prgrmz07@163.com>
2024-06-11 19:45:18 +0800

    测试材料1

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,22 +1,6 @@
-#include "MyClz.h"
-#include <algorithm>
-#include <list>
 
 int main(int argc, char** argv){
-  my_nsp::MyClass varMyClass;
-  static Point pnt1;
-  struct Point pnt2;
-  {
-    struct Point * ptr1=&pnt1;
-    struct Point * ptr2=&pnt2;
-    struct Point pnt3;
-  }
-
-
-  std::list<my_nsp::MyClass*> ls;
-
-  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
-
+  extern void func02();
   return 0;
 }
 
