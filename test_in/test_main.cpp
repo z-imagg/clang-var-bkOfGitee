ca391f9e064362aabd1a5156c5b21a9196c6325c
prgrmz07 <prgrmz07@163.com>
2024-06-11 21:43:12 +0800

    测试结果正常,已跳过函数体内的函数签名语句

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,7 +1,8 @@
+#pragma message("VarBE_inserted")
 
-int main(int argc, char** argv){
-  extern void func02();
-  return 0;
+int main(int argc, char** argv){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "main", 2, 32); /* 初始化函数变量列表, */
+  extern void func02();// 这里没有createVar__RtCxx ,即为 正常, 已跳过函数体内的函数签名语句
+  destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 0;
 }
 
 
