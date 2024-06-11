21f77b335dea0b22c2dcf4c07fbf45df24afb02f
prgrmz07 <prgrmz07@163.com>
2024-06-11 23:28:44 +0800

    [问题记录] 测试材料 : c++ 闭包. (估计是FnVisit没认识闭包)
    这里插入了destroy, 但是却没有插入init. 说明 FnVisit没认识闭包

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,21 +1,36 @@
-#include "MyClz.h"
-#include <algorithm>
-#include <list>
+// c++ 闭包
+// 模拟  jdk-jdk-24-0/src/hotspot/share/gc/g1/g1HeapVerifier.cpp
+class HeapRegionClosure{};
+class G1HeapRegion{};
+int heap_region_iterate(){
+  return 0;
+}
+class G1HeapVerifier{
+public:
+  void verify_bitmap_clear(bool from_tams) ;
+};
 
-int main(int argc, char** argv){
-  my_nsp::MyClass varMyClass;
-  static Point pnt1;
-  struct Point pnt2;
-  {
-    struct Point * ptr1=&pnt1;
-    struct Point * ptr2=&pnt2;
-    struct Point pnt3;
+void G1HeapVerifier::verify_bitmap_clear(bool from_tams) {
+  if (true) {
+    return;
   }
 
+  class G1VerifyBitmapClear : public HeapRegionClosure {
+    bool _from_tams;
 
-  std::list<my_nsp::MyClass*> ls;
+  public:
+    G1VerifyBitmapClear(bool from_tams) : _from_tams(from_tams) { }
 
-  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
+    virtual bool do_heap_region(G1HeapRegion* r) {
+      //这里插入了destroy, 但是却没有插入init. 说明 FnVisit没认识闭包
+      return false;
+    }
+  } cl(from_tams);
+
+  heap_region_iterate();
+}
+
+int main(int argc, char** argv){
 
   return 0;
 }
