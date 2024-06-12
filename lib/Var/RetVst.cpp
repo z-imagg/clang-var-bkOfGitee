9720fd43bfaf270a937c4e6292823ed1d21a60fd
prgrmz07 <prgrmz07@163.com>
2024-06-12 12:41:07 +0800

    [RetVst风格性调整]  用宏语句表达 不可提前退出上层遍历循环

--- lib/Var/RetVst.cpp
+++ lib/Var/RetVst.cpp
@@ -8,6 +8,7 @@
 #include "base/ASTContextUtil.h"
 #include "Var/RangeHasMacroAstVst.h"
 #include "Var/CollectIncMacro_PPCb.h"
+#include "Var/Common.h"
 #include <vector>
 
 #include <fmt/core.h>
@@ -51,7 +52,7 @@ bool RetVst::TraverseReturnStmt(ReturnStmt *returnStmt){
   //跳过非MainFile
   bool _LocFileIDEqMainFileID=UtilMainFile::LocFileIDEqMainFileID(SM,returnStmt->getBeginLoc());
   if(!_LocFileIDEqMainFileID){
-    return false;
+    RetTrue_to_KeepOuterLoop;
   }
 
 
@@ -63,25 +64,29 @@ bool RetVst::TraverseReturnStmt(ReturnStmt *returnStmt){
 
 /////////////////////////对当前节点returnStmt做 自定义处理
   //  Ctx.langOpts.CPlusPlus 估计只能表示当前是用clang++编译的、还是clang编译的, [TODO] 但不能涵盖 'extern c'、'extern c++'造成的语言变化
-  bool useCxx=ASTContextUtil::useCxx(Ctx);
-
-//  int64_t returnStmtID = returnStmt->getID(*Ctx);
-  const SourceLocation &retBgnLoc = returnStmt->getBeginLoc();
-  LocId retBgnLocId=LocId::buildFor(filePath,   retBgnLoc, SM);
-
-  if(bool parentIsCompound=UtilParentKind::parentIsCompound(Ctx,returnStmt)){
-      if(UtilLocId::LocIdSetNotContains(retBgnLocIdSet, retBgnLocId)) {//防重复
-          insert_destroy__Before_fnRet(useCxx, retBgnLocId, retBgnLoc);
-      }
-  }
-
+  do__modify_me(returnStmt, filePath);
+  RetTrue_to_KeepOuterLoop;//上层遍历循环保持继续以免跳过后续源码文本
 ///////////////////// 自定义处理 完毕
 
 ////////////////////  粘接直接子节点到递归链条:  对 当前节点doStmt的下一层节点child:{body} 调用顶层方法TraverseStmt(child)
 //粘接直接子节点到递归链条: TODO: 这段不知道怎么写（得获得return xxx; 的xxx中可能的lambda表达式，并遍历该lambda表达式)， 也有可能不用写：
 //希望return true能继续遍历子节点吧，因为return中应该可以写lambda，lambada内有更复杂的函数结构
-  return true;
 //  Expr *xxx = returnStmt->getRetValue();
+}
+
+void RetVst::do__modify_me(ReturnStmt *returnStmt,std::string filePath){
+  bool useCxx=ASTContextUtil::useCxx(Ctx);
+
+  const SourceLocation &retBgnLoc = returnStmt->getBeginLoc();
+  LocId retBgnLocId=LocId::buildFor(filePath,   retBgnLoc, SM);
+
+  if(bool parentIsCompound=UtilParentKind::parentIsCompound(Ctx,returnStmt)){
+    if(UtilLocId::LocIdSetNotContains(retBgnLocIdSet, retBgnLocId)) {//防重复
+      insert_destroy__Before_fnRet(useCxx, retBgnLocId, retBgnLoc);
+    }
+  }
+
+  return;
 }
 
 
