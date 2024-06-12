24e4ec8ee2149991965711841cc904de9b26ea34
prgrmz07 <prgrmz07@163.com>
2024-06-12 08:37:04 +0800

    测试材料精简

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,10 +1,5 @@
 // c++ 闭包
 // 模拟  jdk-jdk-24-0/src/hotspot/share/gc/g1/g1HeapVerifier.cpp
-class HeapRegionClosure{};
-class G1HeapRegion{};
-int heap_region_iterate(){
-  return 0;
-}
 class G1HeapVerifier{
 public:
   void verify_bitmap_clear(bool from_tams) ;
@@ -15,25 +10,20 @@ void G1HeapVerifier::verify_bitmap_clear(bool from_tams) {
     return;
   }
 
-  class G1VerifyBitmapClear : public HeapRegionClosure {
-    bool _from_tams;
+  class G1VerifyBitmapClear   {
 
   public:
-    G1VerifyBitmapClear(bool from_tams) : _from_tams(from_tams) { }
+    G1VerifyBitmapClear( int xxx)  { }
 
-    virtual bool do_heap_region(G1HeapRegion* r) {
+    virtual bool do_heap_region(int* r) {
       //这里插入了destroy, 但是却没有插入init. 说明 FnVisit没认识闭包
       return false;
     }
   } cl(from_tams);
 
-  heap_region_iterate();
+  double ddyy;
 }
 
-int main(int argc, char** argv){
-
-  return 0;
-}
 
 
 
