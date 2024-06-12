89a72bc2c922a0d8a74b35dcc5b588ceaa48631d
prgrmz07 <prgrmz07@163.com>
2024-06-12 15:35:28 +0800

    [问题][不提前退出上层循环后引发的副作用]FnVst判断了空函数而RetVst没判断空函数 引发不平衡(无init但有destory)
    FnVst判断了空函数而没插入init完事后保持了上层循环, 而RetVst没判断空函数导致插入destroy, 引发不平衡(无init但有destory)

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,21 +1,6 @@
-#include "MyClz.h"
-#include <algorithm>
-#include <list>
 
+char fun01(){return 'X';}
 int main(int argc, char** argv){
-  my_nsp::MyClass varMyClass;
-  static Point pnt1;
-  struct Point pnt2;
-  {
-    struct Point * ptr1=&pnt1;
-    struct Point * ptr2=&pnt2;
-    struct Point pnt3;
-  }
-
-
-  std::list<my_nsp::MyClass*> ls;
-
-  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
 
   return 0;
 }
