a862b8bfa98b5be6ac867bba15517187f9c39cd8
prgrmz07 <prgrmz07@163.com>
2024-06-13 18:45:05 +0800

    测试材料2

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -3,8 +3,14 @@ class T_Animal{};
 class C_User{
 public:
   int func01(Score x);
+  void func02();
 };
 
+class NameList{};
+void C_User::func02() {
+  NameList pipeline_reads;
+  return;
+}
 int C_User::func01(Score x) {
   if(true){
     Score zz;
