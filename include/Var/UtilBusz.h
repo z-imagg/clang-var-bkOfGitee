f645c46327f3b451d9c7eb806b9e4f715e723e53
prgrmz07 <prgrmz07@163.com>
2024-06-14 13:59:38 +0800

    调整 .h 中函数声明位置 以和 .cpp中顺序一致

--- include/Var/UtilBusz.h
+++ include/Var/UtilBusz.h
@@ -10,6 +10,16 @@ using namespace clang;
 
 class UtilBusz {
 public:
+/** 给定语句所在函数声明 是否 为 能够被修改的函数
+ *
+ * @param stmt
+ * @param CI
+ * @param SM
+ * @param Ctx
+ * @return
+ */
+  static bool func_of_stmt_isModifiable(const Stmt* stmt,CompoundStmt* & compoundStmt_/*出参*/, SourceLocation & funcBodyLBraceLoc_/*出参*/, SourceLocation & funcBodyRBraceLoc_/*出参*/, CompilerInstance& CI, SourceManager& SM , ASTContext *Ctx );
+
   /** 给定函数声明 是否 为 能够被修改的函数
  Modifiable == 能够被修改的函数 == 具有被修改资格的函数
  Modifiable 且 该函数还没被修改 == 应该修改该函数
@@ -20,15 +30,6 @@ public:
  */
   static bool isModifiable_FunctionDecl(const FunctionDecl* cxxMethDecl, CompoundStmt* & compoundStmt_/*出参*/, SourceLocation & funcBodyLBraceLoc_/*出参*/, SourceLocation & funcBodyRBraceLoc_/*出参*/,   SourceManager& SM );
 
-/** 给定语句所在函数声明 是否 为 能够被修改的函数
- *
- * @param stmt
- * @param CI
- * @param SM
- * @param Ctx
- * @return
- */
-static bool func_of_stmt_isModifiable(const Stmt* stmt,CompoundStmt* & compoundStmt_/*出参*/, SourceLocation & funcBodyLBraceLoc_/*出参*/, SourceLocation & funcBodyRBraceLoc_/*出参*/, CompilerInstance& CI, SourceManager& SM , ASTContext *Ctx );
 };
 
 
