77986ca45f7fbd1bf819ab645592543757934f4e
prgrmz07 <prgrmz07@163.com>
2024-06-14 18:39:03 +0800

    还原到 简化材料4；  [复现条件]  lambda函数外附近有条件编译宏
    This reverts commit ef7bd3f634b0cc5612efe8cbd1290b03d77ac05d.
    
    Revert "简化材料5"
    
    This reverts commit 99f51bf8dfd20463d27b3f4ffa5e7da8dfa9af4b.

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,4 +1,3 @@
-#pragma message("VarBE_inserted")
 
 class User{};
 class Main{
@@ -10,12 +9,15 @@ int func02(double (*func_ptr) (char,int )){
   int age;
   func_ptr(sex, age);
 }
-double Main::func01(){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "Main::func01", 12, 22); /* 初始化函数变量列表, */
+double Main::func01(){
 
 #ifdef  XXX
   User user;
   User & u = user;
 #endif
-      User self;createVar__RtCxx(_vdLs/*_VarDeclLs*/,  "class User"/*varTypeName*/, sizeof(self)/*varSize*/ , false/*varIsArr*/ , -1/*arrEleSize*/ )  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:18,16 */ ;  
-  destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 0.0;
+    func02([](char ch,int int_){
+      User self;
+      return 1.0;
+    });
+  return 0.0;
 }
