ef7bd3f634b0cc5612efe8cbd1290b03d77ac05d
prgrmz07 <prgrmz07@163.com>
2024-06-14 18:37:54 +0800

    过度简化 导致问题消失

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,3 +1,4 @@
+#pragma message("VarBE_inserted")
 
 class User{};
 class Main{
@@ -9,12 +10,12 @@ int func02(double (*func_ptr) (char,int )){
   int age;
   func_ptr(sex, age);
 }
-double Main::func01(){
+double Main::func01(){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "Main::func01", 12, 22); /* 初始化函数变量列表, */
 
 #ifdef  XXX
   User user;
   User & u = user;
 #endif
-      User self;
-  return 0.0;
+      User self;createVar__RtCxx(_vdLs/*_VarDeclLs*/,  "class User"/*varTypeName*/, sizeof(self)/*varSize*/ , false/*varIsArr*/ , -1/*arrEleSize*/ )  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:18,16 */ ;  
+  destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 0.0;
 }
