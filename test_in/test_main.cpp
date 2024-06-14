20c037e8c7883299236770c65f1ea760f54b8b0b
prgrmz07 <prgrmz07@163.com>
2024-06-14 20:33:57 +0800

    恢复测试材料

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,25 +1,19 @@
-#pragma message("VarBE_inserted")
-
-#define  XXX
-class User{};
-class Main{
+class Score {};
+class T_Animal{};
+class C_User{
 public:
-  double func01();
+  int func01(Score x);
+  void func02();
 };
-int func02(double (*func_ptr) (char,int )){
-  char sex;
-  int age;
-  func_ptr(sex, age);
-}
-double Main::func01(){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "Main::func01", 13, 22); /* 初始化函数变量列表, */
 
-#ifdef  XXX
-  User user;createVar__RtCxx(_vdLs/*_VarDeclLs*/,  "class User"/*varTypeName*/, sizeof(user)/*varSize*/ , false/*varIsArr*/ , -1/*arrEleSize*/ )  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:16,12 */ ;  
-  User & u = user;createVar__RtCxx(_vdLs/*_VarDeclLs*/,  "class User &"/*varTypeName*/, sizeof(u)/*varSize*/ , false/*varIsArr*/ , -1/*arrEleSize*/ )  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:17,18 */ ;  
-#endif
-    func02([](char ch,int int_){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "", 19, 32); /* 初始化函数变量列表, */
-      User self;createVar__RtCxx(_vdLs/*_VarDeclLs*/,  "class User"/*varTypeName*/, sizeof(self)/*varSize*/ , false/*varIsArr*/ , -1/*arrEleSize*/ )  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:20,16 */ ;  
-      destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 1.0;
-    });
-  destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 0.0;
+class NameList{};
+void C_User::func02() {
+  return;
+}
+int C_User::func01(Score x) {
+  if(true){
+    Score zz,yy;
+  }
+  T_Animal animal;
+  return 0.;
 }
