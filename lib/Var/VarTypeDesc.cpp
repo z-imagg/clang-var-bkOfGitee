f52eef421e998f1e4cbd33794ecf1e63a27b30b6
prgrmz07 <prgrmz07@163.com>
2024-06-17 16:49:16 +0800

    [提取函数] 计算 是否关注 该函数

--- lib/Var/VarTypeDesc.cpp
+++ lib/Var/VarTypeDesc.cpp
@@ -8,6 +8,14 @@
 #include "Var/VarTypeDesc.h"
 #include "base/UtilTraverseTypeDefChain.h"
 
+// 计算 是否关注 该函数
+static bool _calc_focus(VarTypeFlag vTF , clang::QualType qT){
+// 其实只有 isLambdaType 是需要VarTypeFlag去特定计算的,其余的不需要
+// 关注        (非内置类型               且    非指针类型             且   非lambda类型)         或    记录类型           或      elaborated类型              或   数组类型         或  类类型          或    结构体or类类型              或   联合类型
+  bool focus= ((!qT->isBuiltinType()) && (!qT->isPointerType()) && (!vTF.isLambdaType)  ) || (qT->isRecordType() || qT->isElaboratedTypeSpecifier() || qT->isArrayType() || qT->isClassType() || qT->isStructureOrClassType() || qT->isUnionType() );
+
+  return focus;
+}
 
 void VarTypeDesc::build(clang::QualType _qualType, VarTypeDesc& self/*出量*/){
 
@@ -49,13 +57,9 @@ void VarTypeDesc::build(clang::QualType _qualType, VarTypeDesc& self/*出量*/){
   assert(vTF._inited);
   self.typeName = qT.getAsString();
   self.msg=fmt::format("typeName={}  {}", self.typeName, self.msg );
-//  clang::Type::TypeClass typeClass = qT->getTypeClass();
-//  clang::Type::Record == typeClass;
-// !vTF.isBuiltinType
-// !vTF.isPointerType
 // 其实只有 isLambdaType 是需要VarTypeFlag去特定计算的,其余的不需要
-// 关注        (非内置类型               且    非指针类型             且   非lambda类型)         或    记录类型           或      elaborated类型              或   数组类型         或  类类型          或    结构体or类类型              或   联合类型
-  self.focus= ((!qT->isBuiltinType()) && (!qT->isPointerType()) && (!vTF.isLambdaType)  ) || (qT->isRecordType() || qT->isElaboratedTypeSpecifier() || qT->isArrayType() || qT->isClassType() || qT->isStructureOrClassType() || qT->isUnionType() );
+// 计算 是否关注 该函数
+  self.focus=_calc_focus(vTF, qT );
 
 //不关注 auto lambda
 //不关注 基本类型
