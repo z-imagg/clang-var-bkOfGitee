d8224c60ec7b96c59d62f513e35eee6445ee25fb
prgrmz07 <prgrmz07@163.com>
2024-06-24 19:31:56 +0800

    [bug , while循环变量] 测试材料

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,23 +1,9 @@
-
-class User{};
-class Main{
-public:
-  double func01();
-};
-int func02(double (*func_ptr) (char,int )){
-  char sex;
-  int age;
-  func_ptr(sex, age);
-}
-double Main::func01(){
-
-#ifdef  XXX
-  User user;
-  User & u = user;
-#endif
-    func02([](char ch,int int_){
-      User self;
-      return 1.0;
-    });
-  return 0.0;
+class T_User {};
+int main(int argc, char** argv){
+  T_User zhangsan;
+  int s;
+  while(T_User * ptr=&zhangsan){
+      s++;
+  }
+  return 0;
 }
